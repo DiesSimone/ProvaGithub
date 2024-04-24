db24356ef83668234c7189ce6811ea0c764104f8
raresymon <raresymon@gmail.com>
2024-04-24 18:14:16 +0200

    Aggiungere i file di progetto.

---
+++ ProvaGithub/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ProvaGithub
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("CIAO GITHUB!!!!!!!!!!!!!!!!!!!");
+        }
+    }
+}
