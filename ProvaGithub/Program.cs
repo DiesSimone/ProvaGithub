6ddc56ae4b0c3f119763f7342a024d58fae32cad
raresymon <raresymon@gmail.com>
2024-04-24 18:31:19 +0200

    dovrebbe uploadare le modifiche? aggiunto una funzione chiamata spam che viene chiamata 1000 volte con un for nel main

--- ProvaGithub/Program.cs
+++ ProvaGithub/Program.cs
@@ -1,8 +1,17 @@
-﻿namespace ProvaGithub
+﻿using System.Runtime.InteropServices.Marshalling;
+
+namespace ProvaGithub
 {
     internal class Program
     {
         static void Main(string[] args)
+        {
+            for (int i = 0; i < 1000; i++) 
+            {
+                Spam();
+            }
+        }
+        public static void Spam()
         {
             Console.WriteLine("CIAO GITHUB!!!!!!!!!!!!!!!!!!!");
         }
